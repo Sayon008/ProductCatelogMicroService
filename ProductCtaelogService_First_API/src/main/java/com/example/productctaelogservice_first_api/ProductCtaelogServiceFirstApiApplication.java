6c05a29a1b8af64681e6378974808394bd087029
Sayon Batabyal <batabyalsayon@gmail.com>
2025-01-24 11:18:57 +0530

    MicroService: ProductCatelogService Full Implementation

---
+++ ProductCtaelogService_First_API/src/main/java/com/example/productctaelogservice_first_api/ProductCtaelogServiceFirstApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.productctaelogservice_first_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCtaelogServiceFirstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCtaelogServiceFirstApiApplication.class, args);
+	}
+
+}
---
+++ ProductCtaelogService_First_API/src/test/java/com/example/productctaelogservice_first_api/ProductCtaelogServiceFirstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productctaelogservice_first_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCtaelogServiceFirstApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
